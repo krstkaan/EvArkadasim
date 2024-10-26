47d2fe313413cd4c3841a8fdaf00492597319780
krstkaan <k.kaanakbulak@gmail.com>
2024-10-26 21:43:10 +0300

    Refactor code structure and add navigation
    - Refactored code structure to separate the HomePage component into its own file
    - Added navigation using @react-navigation/native-stack and @react-navigation/stack packages
    - Updated dependencies in package.json to include react-redux and redux

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomePage from './src/screens/HomePage';
+const Stack = createStackNavigator();
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Ev Arkadaşım</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomePage} options={{ headerShown: false,}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
